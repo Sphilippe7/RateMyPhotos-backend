093bcd4eef98241115b992bc0f90f47e840e13e5
sphilippe7 <solomon924@gmail.com>
2017-01-18 21:01:18 -0500

    Tested curl requests
    Just made files and tested curl requests for the update function

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get '/photos', to: 'photos#index'
   post '/photos', to: 'photos#create'
   get '/photos/:id', to: 'photos#show'
-  patch '/photos/:id', to: 'photos#update'
+  put '/photos/:id', to: 'photos#update'
   delete '/photos/:id', to: 'photos#destroy'
 end
--- scripts/get-photos.sh
+++ scripts/get-photos.sh
@@ -1,5 +1,21 @@
+
 API="http://localhost:4741"
 URL_PATH="/photos"
-curl "${API}${URL_PATH}/$ID" \
+ID=3
+NAME="Stanley"
+CATEGORY="Portrait"
+TOKEN="BAhJIiU3ODc1OGUxNDYxNmZlMzUzZGE0YmZmNjY4Njk2YThjZgY6BkVG--ab2d13577e1fdddf1169720e67f094e99a132567"
+URL="http://i.imgur.com/8G9PX6J.jpg"
+
+curl "${API}${URL_PATH}/${ID}" \
   --include \
-  --request GET
+  --request PUT \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "photo": {
+      "name": "'"${NAME}"'",
+      "category": "'"${CATEGORY}"'",
+      "url": "'"${URL}"'"
+    }
+  }'
